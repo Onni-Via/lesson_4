7f0f0dbe95c2ee39dc96ef20f29b4ccec2f96661
Elena Kryuchkina <ekryuchkina@hse.ru>
2022-03-20 18:45:36 +0300

    на проверку

--- user_gallery/index.php
+++ user_gallery/index.php
@@ -1,19 +1,10 @@
 <?php
+$validType = '';
 if (isset($_FILES['myimg']) ) {
-    if (0 == $_FILES['myimg']['error']) {
+    if (0 == $_FILES['myimg']['error'] && $_FILES['myimg']['type'] === 'image/png' || $_FILES['myimg']['type'] === 'image/jpeg') {
     $res = move_uploaded_file($_FILES['myimg']['tmp_name'], __DIR__ . '/images/' . $_FILES['myimg']['name'] . $validType);
     }
 }
-
-$validType = '0';
-if ($_FILES['myimg']['type'] == 'image/png') {
-	$validType = '.png';
-} elseif ($_FILES['myimg']['type'] == 'image/jpeg') {
-	$validType = '.jpg';
-} else { 
-	$res = 'Не допустимый тип файла';
-	echo $res; // я ожидаю что файлы загружатбся не будут, вместо этого получу строку, но мой код все равно файл загружает+строка. Почему?
- } 
 
 ?>
 <!DOCTYPE HTML>
