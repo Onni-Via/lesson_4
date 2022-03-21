50dbfc99a1c288575e496c23ea87f2e7d5077b39
Elena Kryuchkina <ekryuchkina@hse.ru>
2022-03-21 17:36:33 +0300

    файлы

--- user_gallery/index.php
+++ user_gallery/index.php
@@ -3,15 +3,6 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-$message = '';
-if (isset($_FILES['myimg']) ) {
-    if (0 == $_FILES['myimg']['error'] && $_FILES['myimg']['type'] === 'image/png' || $_FILES['myimg']['type'] === 'image/jpeg') {
-    $res = move_uploaded_file($_FILES['myimg']['tmp_name'], __DIR__ . '/images/' . $_FILES['myimg']['name']);
-    } else {
-        $message = "Ошибка загрузки изображения";
-    }
-}
-
 $imgNamesArray = scandir(__DIR__ . '/images/');
 
 $superfluous = ['.', '..'];
@@ -25,11 +16,11 @@ $imgNamesArray = array_diff($imgNamesArray, $superfluous); // в мануале 
   <title>Примитивная фотогалерея</title>
 </head>
 <body>
-<form action="" method="post" enctype="multipart/form-data">
+<form action="/sprint_f/lesson_4/user_gallery/upload.php" 
+method="post" enctype="multipart/form-data">
 <input type="file" name="myimg">
 <button type="submit">Загрузить</button></form>
-<?php echo $message; 
-foreach ($imgNamesArray as $imgNames) { ?>
+<?php foreach ($imgNamesArray as $imgNames) { ?>
 </br>
 <img src="http://localhost/sprint_f/lesson_4/user_gallery/images/<?php echo $imgNames; ?> " height="300px"> 
 <?php } ?>
---
+++ user_gallery/upload.php
@@ -0,0 +1,10 @@
+<?php
+$message = '';
+if (isset($_FILES['myimg']) ) {
+    if (0 == $_FILES['myimg']['error'] && $_FILES['myimg']['type'] === 'image/png' || $_FILES['myimg']['type'] === 'image/jpeg') {
+    $res = move_uploaded_file($_FILES['myimg']['tmp_name'], __DIR__ . '/images/' . $_FILES['myimg']['name']);
+    } else {
+        echo "Ошибка загрузки изображения";
+    }
+}
+header('Location: http://localhost/sprint_f/lesson_4/user_gallery/index.php');
