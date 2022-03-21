c47d28e9d76ba3fb62226de3aaeae5aee4270ba5
Elena Kryuchkina <ekryuchkina@hse.ru>
2022-03-21 13:01:13 +0300

    код в один файл, html в другой

--- user_gallery/index.php
+++ user_gallery/index.php
@@ -1,9 +1,5 @@
 <?php
-if (isset($_FILES['myimg']) ) {
-    if (0 == $_FILES['myimg']['error'] && $_FILES['myimg']['type'] === 'image/png' || $_FILES['myimg']['type'] === 'image/jpeg') {
-    $res = move_uploaded_file($_FILES['myimg']['tmp_name'], __DIR__ . '/images/' . $_FILES['myimg']['name']);
-    }
-}
+include __DIR__ . '/upload.php';
 
 ?>
 <!DOCTYPE HTML>
@@ -16,5 +12,8 @@ if (isset($_FILES['myimg']) ) {
 <form action="" method="post" enctype="multipart/form-data">
 <input type="file" name="myimg">
 <button type="submit">Загрузить</button></form>
+<?php foreach ($imgNamesArray as $imgNames) { ?>
+<img src="http://localhost/sprint_f/lesson_4/user_gallery/images/<?php echo $imgNames; ?> " height="200px">
+<?php  } ?>
 </body>
 </html>
